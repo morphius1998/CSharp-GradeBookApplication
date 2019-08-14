943e07dbdf6e01168ad4a0e091236e3270a8bb3d
morphius1998 <lbost@comcast.net>
2019-08-13 23:51:04 -0500

    Completed the module

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -111,20 +111,34 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            double gradePoint = 0;
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    gradePoint = 4;
+                    break;
                 case 'B':
-                    return 3;
+                    gradePoint = 3;
+                    break;
                 case 'C':
-                    return 2;
+                    gradePoint = 2;
+                    break;
                 case 'D':
-                    return 1;
+                    gradePoint = 1;
+                    break;
                 case 'F':
-                    return 0;
+                    gradePoint = 0;
+                    break;
             }
-            return 0;
+
+            if (IsWeighted &&
+                (studentType == StudentType.Honors ||
+                 studentType == StudentType.DualEnrolled))
+                {
+                gradePoint++;
+            }
+
+            return gradePoint;
         }
 
         public virtual void CalculateStatistics()
